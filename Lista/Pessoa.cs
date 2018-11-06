43a76dc9dfbbfd6700c5412d45e18e4c57475bfa
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 09:06:03 -0300

    Program Lista

---
+++ Lista/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Lista
+{
+    class Funcionario
+    {
+        public string Nome { get; private set; }
+        public string Cpf { get; private set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(string Nome, string Cpf, double Salario)
+        {
+            this.Nome = Nome;
+            this.Cpf = Cpf;
+            this.Salario = Salario;
+        }
+
+        public void Aumentarsalario(double porcentagem)
+        {
+            Salario = porcentagem * Salario / 100.0 + Salario;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " 
+                + Nome
+                + ", "
+                + "CPF: "
+                + Cpf
+                + ", "
+                + "Salario: R$"
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Lista/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M;
+
+            Console.Write("Digite a quantidade de funcionarios: ");
+            M = int.Parse(Console.ReadLine());
+
+            List<Funcionario> lista = new List<Funcionario>();
+
+            for (int i = 1; i<=M; i++)
+            {
+                Console.WriteLine("Digite os dados do " + i + "º funcionario: ");
+                Console.Write("Nome: ");
+                string Nome = Console.ReadLine();
+                Console.Write("CPF: ");
+                string Cpf = Console.ReadLine();
+                Console.Write("Salario: R$");
+                double Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Funcionario F = new Funcionario(Nome, Cpf, Salario);
+                lista.Add(F);
+            }
+
+            Console.WriteLine(); 
+            Console.Write("Digite o CPF do funcionario que reberá aumento: ");
+            string CpfBuscado = Console.ReadLine();
+
+            int pos = lista.FindIndex(x => x.Cpf == CpfBuscado);
+
+            if (pos == -1)
+            {
+                Console.WriteLine("CPF INEXISTENTE!");
+            }
+            else
+            {
+                Console.Write("Digite a porcentagem de aumento: ");
+                double porc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                lista[pos].Aumentarsalario(porc);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Informações gerais dos funcionarios: ");
+
+            for (int i=0; i<lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
